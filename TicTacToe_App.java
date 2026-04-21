e44d3ea794baa63ce40d87a6d9f3f94857a6835b
Caffeinelikeshani <76482524+caffeinelikeshani@users.noreply.github.com>
2026-04-21 14:32:47 +0530

    Add files via upload

---
+++ TicTacToe_App.java
@@ -0,0 +1,19 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        System.out.println("Current Board Layout:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println(); // Move to the next line after each row
+        }
+    }
+}
