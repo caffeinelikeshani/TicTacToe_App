1a866e0ccd50a49782bb6155f006898166226799
Caffeinelikeshani <76482524+caffeinelikeshani@users.noreply.github.com>
2026-04-21 14:34:45 +0530

    Add files via upload

--- TicTacToe_App.java
+++ TicTacToe_App.java
@@ -1,19 +1,29 @@
-public class TicTacToe {
+import java.util.Random;
+
+public class GameSetup {
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
+        String player1 = "Player 1";
+        String player2 = "Player 2";
+        
+        String currentPlayer;
+        String p1Symbol;
+        String p2Symbol;
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+        Random random = new Random();
+        int toss = random.nextInt(2);
+
+        if (toss == 0) {
+            currentPlayer = player1;
+            p1Symbol = "X";
+            p2Symbol = "O";
+        } else {
+            currentPlayer = player2;
+            p1Symbol = "O";
+            p2Symbol = "X";
         }
 
-        System.out.println("Current Board Layout:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println(); // Move to the next line after each row
-        }
+        System.out.println("Toss result: " + currentPlayer + " goes first!");
+        System.out.println(player1 + " is " + p1Symbol);
+        System.out.println(player2 + " is " + p2Symbol);
     }
 }
