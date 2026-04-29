e29e682306faaeeadda4ec3f4160656ddd8bf5d4
Caffeinelikeshani <76482524+caffeinelikeshani@users.noreply.github.com>
2026-04-29 14:33:48 +0530

    Add files via upload

--- TicTacToe_App.java
+++ TicTacToe_App.java
@@ -1,19 +1,42 @@
-public class TicTacToe {
+public class GameLoop {
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
+        char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+        };
+        
+        boolean gameRunning = true;
+        char currentPlayer = 'X';
+        int movesMade = 0;
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+        while (gameRunning) {
+            System.out.println("Current turn: " + currentPlayer);
+            
+            // Logic to get move, validate, and update board would go here
+            // Example: updateBoard(board, row, col, currentPlayer);
+            movesMade++;
+
+            if (checkWin(board, currentPlayer)) {
+                System.out.println("Player " + currentPlayer + " wins!");
+                gameRunning = false;
+            } else if (movesMade == 9) {
+                System.out.println("It's a draw!");
+                gameRunning = false;
+            } else {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
             }
         }
+    }
 
-        System.out.println("Current Board Layout:");
+    public static boolean checkWin(char[][] board, char symbol) {
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
+            if ((board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) ||
+                (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol)) {
+                return true;
             }
-            System.out.println(); // Move to the next line after each row
         }
+        return (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||
+               (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol);
     }
 }
