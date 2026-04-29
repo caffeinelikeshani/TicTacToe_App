9d4156a2424c2a674b98827732a715ceffd52f68
Caffeinelikeshani <76482524+caffeinelikeshani@users.noreply.github.com>
2026-04-29 14:23:01 +0530

    Add files via upload

--- TicTacToe_App.java
+++ TicTacToe_App.java
@@ -1,19 +1,26 @@
-public class TicTacToe {
-    public static void main(String[] args) {
-        char[][] board = new char[3][3];
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+public class MoveValidator {
+    public static boolean isValidMove(char[][] board, int row, int col) {
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
         }
+        
+        return board[row][col] == '-';
+    }
 
-        System.out.println("Current Board Layout:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println(); // Move to the next line after each row
+    public static void main(String[] args) {
+        char[][] board = {
+            {'-', '-', '-'},
+            {'-', 'X', '-'},
+            {'-', '-', '-'}
+        };
+
+        int testRow = 1;
+        int testCol = 1;
+
+        if (isValidMove(board, testRow, testCol)) {
+            System.out.println("Move is valid.");
+        } else {
+            System.out.println("Move is invalid.");
         }
     }
 }
